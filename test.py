1c72518243b4f0cbbf36f2d3f617eea13be096fa
Devam Mondal <devammondal06@gmail.com>
2023-11-18 16:57:36 -0500

    created db-index front end

--- test.py
+++ test.py
@@ -47,9 +47,9 @@ def datasets():
     datasets = Datasets.query
     return render_template('datasets.html', title='Dataset Bias Leaderboard', datasets=datasets)
 
-@app.route("/calculatedb")
-def calculate_db_index():
-    return {"message": "this is an endpoint to calculate db-index"}
+@app.route("/dbindex")
+def dbindex(methods=["GET", "POST"]):
+    return render_template('dbindex.html')
 
 @app.route("/llms")
 def llms():
