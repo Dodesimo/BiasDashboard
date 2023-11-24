b0d1014318750da954400b0fef182fe0d1516290
Devam Mondal <devammondal06@gmail.com>
2023-11-24 14:27:48 -0500

    datasets can be added to leaderboard through db-index analysis

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from dbindex import test as t
 from dbindex import dbIndex, test
+from datasets import load_dataset
+from leaderboardupload import dsetGetName, dsetGetSource, dsetGetSize
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -14,7 +16,7 @@ class Datasets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), index=True)
     size = db.Column(db.String(4), index=True)
-    source = db.Column(db.String(256), index=True)
+    creator = db.Column(db.String(256), index=True)
     db_index = db.Column(db.Double, index=True)
     rdb_index = db.Column(db.Double, index=True)
     gdb_index = db.Column(db.Double, index=True)
@@ -25,7 +27,7 @@ class LLMS(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), index=True)
     size = db.Column(db.String(4), index=True)
-    source = db.Column(db.String(256), index=True)
+    creator = db.Column(db.String(256), index=True)
     glue = db.Column(db.Double, index=True)
     stereoset = db.Column(db.Double, index=True)
     mb_index = db.Column(db.Double, index=True)
@@ -35,12 +37,12 @@ with app.app_context():
     db.create_all()
 
     # Sample record creation
-    dataset1 = Datasets(id=0, name="RedPajama-Data-V2", size=37, source="togethercomputer", db_index=0, rdb_index=2,
+    dataset1 = Datasets(id=0, name="RedPajama-Data-V2", size=37, creator="togethercomputer", db_index=0, rdb_index=2,
                         gdb_index=2, pdb_index=3)
-    dataset2 = Datasets(id=1, name="ultrachat_200k", size=200000, source="HuggingFaceH4", db_index=0, rdb_index=100,
+    dataset2 = Datasets(id=1, name="ultrachat_200k", size=200000, creator="HuggingFaceH4", db_index=0, rdb_index=100,
                         gdb_index=2, pdb_index=30)
-    llm1 = LLMS(id=0, name="Yi-34B", size="34B", source="01-ai", glue=0, stereoset=2, mb_index=2)
-    llm2 = LLMS(id=1, name="OpenChat 3.5", size="7B", source="OpenChat", glue=0, stereoset=4, mb_index=1)
+    llm1 = LLMS(id=0, name="Yi-34B", size="34B", creator="01-ai", glue=0, stereoset=2, mb_index=2)
+    llm2 = LLMS(id=1, name="OpenChat 3.5", size="7B", creator="OpenChat", glue=0, stereoset=4, mb_index=1)
     db.session.add_all([dataset1, dataset2, llm1, llm2])
     db.session.commit()
 
@@ -64,7 +66,13 @@ def dbindex():
 def calculate_db_index():
     if request.method == "POST":
         link = request.form.get('dsetlabel')
-        return render_template("calculated.html", dbi=dbIndex((link)))
+        dbi = dbIndex((link))
+        dset = Datasets(id=db.session.query(Datasets).count() + 1, name=dsetGetName(link), size=dsetGetSize(link), creator=dsetGetSource(link), db_index=dbi,
+                            rdb_index=2,
+                            gdb_index=2, pdb_index=3)
+        db.session.add(dset)
+        db.session.commit()
+        return render_template("calculated.html", dbi=dbi)
 
 @app.route("/mbindex")
 def mbindex():
